01fde286b4cfc48a791a3d5b677ac43e4d0240ac
gunawanaja <55255158+gunawanaja@users.noreply.github.com>
2021-10-01 16:20:05 +0000

    Add some codes

--- Gunawan ngentoto.php
+++ Gunawan ngentoto.php
@@ -32,3 +32,22 @@ if ($uang < $harga) {
 }
 
 ?>
+
+
+
+<html>
+<head>
+	<title>Php Nasi Goreng gilacoding.com</title>
+</head>
+<body>
+<h1>Belajar PHP (IF/ELSE)</h1>
+<form method="post">
+Uang kita = <input type="text" name="uang" required><br>
+Harga nasi goreng = <input type="text" name="harga" required><br>
+
+
+
+<br><input type="submit" value="Proses">
+</form>
+</body>
+</html>
