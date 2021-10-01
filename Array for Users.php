cbb9e74306454bae901e2bf31233028b76f3ec5c
Dairenya Shou <84019537+dairenyashou@users.noreply.github.com>
2021-10-02 00:50:54 +0700

    Update Array for Users.php
    Added CSS tags for styling page for Users

--- Array for Users.php
+++ Array for Users.php
@@ -9,6 +9,17 @@ $angka = [3,2,15,20,11,77,89];
 <head>
 	<meta charset="utf-8">
 	<title>Exanthenon</title>
+	<style>
+		div {
+			width: 50px;
+			height: 50px;
+			background-color: salmon;
+			text-align: center;
+			line-height: 50px;
+			margin: 3px;
+			float: left;
+		}
+	</style>
 </head>
 <body>
 
