a18115520ce7026c46ae6c9c6368f6953abd3853
Lia Bertresse <84742671+liabertresse@users.noreply.github.com>
2021-10-02 07:27:33 +0700

    Update Nanz Nugraha.php
    Added variable with global scope

--- Nanz Nugraha.php
+++ Nanz Nugraha.php
@@ -11,3 +11,21 @@ echo "My boat is " . $coLOR . "<br>";
 
 </body>
 </html>
+
+<?php
+
+$x = 5; // global scope
+
+function myTest() {
+
+  // using x inside this function will generate an error
+
+  echo "<p>Variable x inside function is: $x</p>";
+
+}
+
+myTest();
+
+echo "<p>Variable x outside function is: $x</p>";
+
+?>
