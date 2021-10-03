97546c82e10fb8a8a1289b629c0f00d082377d86
hldnkstou <91795239+hldnkstou@users.noreply.github.com>
2021-10-03 09:48:13 +0700

    Update hldnkstouxssfilterfunc.php
    update ex xss attack

--- hldnkstouxssfilterfunc.php
+++ hldnkstouxssfilterfunc.php
@@ -5,5 +5,7 @@ $filterxss = strip_tags(htmlspecialchars(htmlentities($data,ENT_QUOTES)));
 return $filter;
 
 }
-  $message = 'developerlawak';
+$message = '<script>alert(1)</script>'; // contoh serangan xss
 	$input = mysqli_real_escape_string(trim(filterxss($_POST['message'])));
+
+
