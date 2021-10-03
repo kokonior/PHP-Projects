57572505e2923e3869dc03ee898d573ceb13947f
sironkurus <sironganteng11@gmail.com>
2021-10-03 10:18:37 +0700

    Update sironkurus.php

--- sironkurus.php
+++ sironkurus.php
@@ -5,7 +5,8 @@
 <?php
 echo "My first PHP script from sironkurus to Hacktoberfest!";
 echo "Hacktoberfest coding2,semoga lancar";
-echo "semoga kaosnya landing, aamiin";
+echo "Semoga kaosnya landing, aamiin";
+echo "Alhamdulillah";
   ?> 
 
 </body>
