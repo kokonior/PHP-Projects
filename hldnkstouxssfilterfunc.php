d33268c6364ed5019f40942307213f5a262e553a
Hildan Kusto Utomo <91795239+hldnkstou@users.noreply.github.com>
2021-10-04 20:28:13 +0700

    Update hldnkstouxssfilterfunc.php
    update new filter

--- hldnkstouxssfilterfunc.php
+++ hldnkstouxssfilterfunc.php
@@ -1,11 +1,11 @@
 function filterxss($data){
 
-$filterxss = strip_tags(htmlspecialchars(htmlentities($data,ENT_QUOTES)));
+$filterxss = strip_slashes(strip_tags(htmlspecialchars(htmlentities($data,ENT_QUOTES))));
 
 return $filter;
 
 }
-$message = '<script>alert(1)</script>'; // contoh serangan xss
+	$message = '<script>alert(1)</script>'; // contoh serangan xss
 	$input = mysqli_real_escape_string(trim(filterxss($_POST['message'])));
-
+echo $message;
 
