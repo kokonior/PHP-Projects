f840270098c7a1f6e065dcae4fc3ba8e35708719
Rizky DPS <rzkydps@gmail.com>
2021-10-04 16:25:44 +0800

    add new function
    function matematika

--- Function return.php
+++ Function return.php
@@ -7,6 +7,46 @@ function salam($name = 'Admin' ) {
 function salam($date = 'Selamat Datang', $name = 'admin') {
 	return "Selamat $date, $name";
 }
+
+
+// Kalkulator Sederhana antara dua bilangan
+$x = 9;
+$y = 3;
+
+
+
+echo "=================================== <br/>";
+// FUngsi penjumlahan
+function functionPenambahan($x, $y)
+{
+    $HasilPenambahan = $x + $y;
+    return $HasilPenambahan;
+}
+// FUngsi Perkalian
+function functionPerkalian($x, $y)
+{
+    $HasilPerkalian = $x * $y;
+    return $HasilPerkalian;
+}
+// Fungsi Pengurangan
+function functionPengurangan($x, $y)
+{
+    $HasilPengurangan = $x - $y;
+    return $HasilPengurangan;
+}
+// Fungsi Pembagian
+function functionPembagian($x, $y)
+{
+    $HasilPembagian = $x / $y;
+    return $HasilPembagian;
+}
+
+// HAsil dari seluruh fungsi (Rizky DWi Putra Sidik)
+echo "Hasil Penjumlahan adalah :" . functionPenambahan(9, 3) . "<br/>";
+echo "Hasil Pengurangan adalah :" . functionPengurangan(9, 3) . "<br/>";
+echo "Hasil Perkalian adalah :"  . functionPerkalian(9, 3) . "<br/>";
+echo "Hasil Penjumlahan adalah :" . functionPembagian(9, 3) . "<br/>";
+
 ?>
 
 <!DOCTYPE html>
