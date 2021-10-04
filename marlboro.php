f075e1a522cce5c7aa2f361670b6aa7d586eaab3
dimasdew <61452341+dimasdew@users.noreply.github.com>
2021-10-04 21:01:47 +0700

    marlboro.php
    Bot

---
+++ marlboro.php
@@ -0,0 +1,5 @@
+<?php $SCHEMA = "marlboro_redrush_2021"
+$SCHEMA_GAME = "marlboro_redrush_game_2021"
+$SCHEMA_REPORT = "marlboro_redrush_report_2021"
+$SCHEMA_REPORT_TEMP = "marlboro_redrush_report_temp_2021" // $SCHEMA = "redrush";// $SCHEMA_GAME = "redrush_game";// $SCHEMA_REPORT = "redrush_report";
+$SCHEMA_REPORT_TEMP = "redrush_report_temp"; $DATABASE[0] =
