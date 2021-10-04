3a66417ebe81788382091e7f06a5e0961f13e63d
Achmad Rivaldi <33930946+mrtampan@users.noreply.github.com>
2021-10-04 17:16:10 +0700

    Create print_text.php

---
+++ print_text.php
@@ -0,0 +1,12 @@
+<?php
+
+$nama = "ucup";
+
+echo "ini namanya si ". $nama; 
+
+print($nama. " pergi kepasar ");
+
+var_dump($nama " mengebom atom hirosima dan nagasaki");
+
+echo "ini cuma cerita karangan jangan diambil pusing, si " . $nama " orang baik dan tidak jahad terhadap siapapun";
+?>
