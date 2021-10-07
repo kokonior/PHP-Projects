4857154ecf3c5dd812a08113c9ad0bd33728c3fd
septyo7 <81677115+septyo7@users.noreply.github.com>
2021-10-07 20:31:19 +0700

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,12 @@
+<!Doctype html>
+<html>
+<body>
+<?php
+echo "Today is".date("Y/m/d")."<br>";
+echo "Today is".date("Y.m.d")."<br>";
+echo "Today is".date("Y-m-d")."<br>";
+echo "Today is".date("1");
+?>
+
+</body>
+</html>
