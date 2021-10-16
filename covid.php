a9a6497b785c888f3b1e66c9b6fceee6384e0713
nanzcakep <43292701+nanzcakep@users.noreply.github.com>
2021-10-16 22:59:18 +0700

    covid.php
    add html

--- covid.php
+++ covid.php
@@ -20,3 +20,22 @@ print_r();
 echo "</pre>";
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Covid Indonesia</title>
+</head>
+<body>
+    
+    <p>Negara : <?= $data[0]['name']; ?></p>
+    <p>Positif : <?= $data[0]['positif']; ?></p>
+    <p>Sembuh : <?= $data[0]['sembuh']; ?></p>
+    <p>Meninggal : <?= $data[0]['meninggal']; ?></p>
+    <p>Dirawat : <?= $data[0]['dirawat']; ?></p>
+
+</body>
+</html>
