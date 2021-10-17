28651ad8ad5bf08dd0f2e1cbe38b30867115b074
andianiputrii <72190229+andianiputrii@users.noreply.github.com>
2021-10-17 21:12:33 +0700

    PHP Function Arguments 2.php

---
+++ PHP Function Arguments 2.php
@@ -0,0 +1,8 @@
+ <?php declare(strict_types=1); // strict requirement
+
+function addNumbers(int $a, int $b) {
+  return $a + $b;
+}
+echo addNumbers(5, "5 days");
+// since strict is enabled and "5 days" is not an integer, an error will be thrown
+?> 
