5e9773d19797ba28f9c4d9db591032a219e5a9b0
Faishal AR <38537356+SantriInsyaf@users.noreply.github.com>
2021-10-17 19:45:59 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,70 @@
+
+<!DOCTYPE html>
+<head>
+ <title>Iseng</title>
+ <!--Bang-->
+ <link href='https://vignette.wikia.nocookie.net/shi-sui/images/1/19/Images_%282%29.jpg/revision/latest?cb=20180127053601' rel='shortcut icon'>
+ <meta property="og:title" content="Design by me">
+ <meta property="og:description" content="Cuman Test">
+ <meta property="og:author" content="Aswad Al Ansori">
+ <meta charset="utf-8">
+ <link href='http://fonts.googleapis.com/css?family=Iceland' rel='stylesheet' type='text/css'>
+ <link href="https://fonts.googleapis.com/css?family=Margarine|Trade+Winds" rel="stylesheet"type='text/css'>
+ <link href="https://fonts.googleapis.com/css?family=Chilanka"rel="stylesheet" type="text/css">
+</head>
+<body bgcolor="black">
+<br/><br/>
+<style>
+body {
+  background: url('https://1.bp.blogspot.com/-0CQECEv2TNM/XXxEbYP-PJI/AAAAAAAAAPI/3ykMe66iZbEGhbkdOn1EOec9FLTcJyvKQCLcBGAsYHQ/s1600/14e15d457c4740c9eabee8058e0c78be.png') no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  color: white;
+}
+.twitter a{
+  text-decoration: none;
+  font-family: Arial, sans-serif  ;
+  font-size: 50px;
+  text-shadow: grey 0px 0px 10px;
+}
+}</style>
+
+<center>
+<img src="https://1.bp.blogspot.com/-qdcww3zGk-g/XXxEMs1ZCYI/AAAAAAAAAPE/M4-EJ_0ycjkTJ-zEpOyKJwUNF8eLjYTYgCLcBGAsYHQ/s1600/Me%2B.JPG" width="250" height="250"></br></a>
+<!--
+<style>
+img{
+animation-name: rotate ;
+animation-duration: 0s;
+animation-play-state: running;
+animation-timing-function: linear;
+animation-iteration-count: infinite;
+}
+@keyframes rotate{
+10% {transform:rotateY(36deg)}
+20% {transform:rotateY(72deg)}
+30% {transform:rotateY(108deg)}
+40% {transform:rotateY(144deg)}
+50% {transform:rotateY(180deg)}
+60% {transform:rotateY(216deg)}
+70% {transform:rotateY(252deg)}
+80% {transform:rotateY(288deg)}
+90% {transform:rotateY(324deg)}
+100% {transform:rotateY(360deg)}
+}
+</style>
+-->
+</center>
+<center>
+<font size="18" color="red" face="Trade Winds"><br><b>My Name Faishal Ananta Ridha</b></br></font></center>
+<center>
+<div id="content1"><font size="8px" color="red" face="Chilanka">
+<p><b>Still Progress</p></div></b></font></center>
+<center><br><font color='white' face="Iceland" size="5px">.:: Contact Me ::.</font><br></center>
+<center><br><a class="link" href="https://fb.me/isallkun.ar" target="_blank" rel="nofollow"><font color='white' face="Iceland" size="5px">Faishal AR</a></font></center>
+</head>
+<script src="https://cdn.rawgit.com/bungfrangki/efeksalju/2a7805c7/efek-salju.js" type="text/javascript"></script>
+<audio src="/music/Anonymouz.mp3" autoplay="autoplay"></audio>
+</html>
