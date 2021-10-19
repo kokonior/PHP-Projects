0ec76c3f95a46bc4bad9dad6dca7663e8ea40f4e
Risky <46136186+riskgaming@users.noreply.github.com>
2021-10-19 14:57:20 +0700

    Update RISKY.php
    Add kelereng bot

--- RISKY.php
+++ RISKY.php
@@ -11,4 +11,21 @@
     $taruhanKelereng;
     $tebakanBot;
 
+echo "\n";
+    echo "Selamat Datang di SquidGame Kelereng \n"; 
+
+    //Get userName
+    echo  "Masukan nama anda : ";
+    $namaUser  = trim(fgets(STDIN));
+    echo  "\n";
+
+    //Check if user fill name or not
+    if($namaUser == TRUE){
+        
+        echo "Hai ".$namaUser." selamat bermain!";
+        echo "\n";
+        echo "Kelereng Bot  : ".$kelerengBot;
+        echo "\n";
+        echo "Kelereng Kamu : ".$kelerengUser;
+        echo "\n";
 ?>
