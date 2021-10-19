f44bbdb77303452b828654ce92ae2faffbf49885
Arya Arriady <71863153+aryarriady@users.noreply.github.com>
2021-10-19 14:47:51 +0700

    Create AryaArriady.php

---
+++ AryaArriady.php
@@ -0,0 +1,10 @@
+<?php
+
+        if(isset($_POST['submit'])){
+            $alas = $_POST['alas'];
+            $tinggi = $_POST['tinggi'];
+            
+            // menghitung luas segitiga
+            $luas_segitiga = 1/2 * $alas * $tinggi;
+          
+?>
