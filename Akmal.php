1855400dbe72c7864384bf471a277e2be6f96e00
akmal <63574967+akmal203@users.noreply.github.com>
2021-10-19 16:57:11 +0700

    Update Akmal.php
    Add fill name

--- Akmal.php
+++ Akmal.php
@@ -19,5 +19,36 @@
     $namaUser  = trim(fgets(STDIN));
     echo  "\n";
 
+//Check if user fill name or not
+    if($namaUser == TRUE){
+        
+        echo "Hai ".$namaUser." selamat bermain!";
+        echo "\n";
+        echo "Kelereng Bot  : ".$kelerengBot;
+        echo "\n";
+        echo "Kelereng Kamu : ".$kelerengUser;
+        echo "\n";
 
+        do{
+            echo "\n";
+
+            //Get Tebakan User
+            echo "Masukan Tebakan (Ganjil/Genap) : ";
+            $tebakanUser = strtolower(trim(fgets(STDIN)));
+            
+            //Get Taruhan User
+            echo "Masukan Taruhan 1 - 10         : ";
+            $taruhanUser = trim(fgets(STDIN));
+            echo "\n";
+
+            //Get Rand number for bot
+            $taruhanKelereng = rand(1,$kelerengBot);
+            
+            //cek ganjil genap
+            if($taruhanKelereng % 2 == 0 ){
+                $tebakanBot = "genap";
+            }else{
+                $tebakanBot = "ganjil";
+            }
+            
 ?>
