0214157c059fba537fc9f52c6dc76c8009335df1
Efraim1209 <92778445+Efraim1209@users.noreply.github.com>
2021-10-19 14:30:56 +0700

    Update Efraim.php
    Add php programs and if else method

--- Efraim.php
+++ Efraim.php
@@ -26,3 +26,31 @@ if (playsCount() >= 9) {
     <table class="tic-tac-toe" cellpadding="0" cellspacing="0">
         <tbody>
         </html>
+
+    <?php
+        $lastRow = 0;
+        for ($i = 1; $i <= 9; $i++) {
+            $row = ceil($i / 3);
+
+            if ($row !== $lastRow) {
+                $lastRow = $row;
+
+                if ($i > 1) {
+                    echo "</tr>";
+                }
+
+                echo "<tr class='row-{$row}'>";
+            }
+
+            $additionalClass = '';
+
+            if ($i == 2 || $i == 8) {
+                $additionalClass = 'vertical-border';
+            }
+            else if ($i == 4 || $i == 6) {
+                $additionalClass = 'horizontal-border';
+            }
+            else if ($i == 5) {
+                $additionalClass = 'center-border';
+            }
+            ?>
