3d9f9cf4e0acd75ad15d2c5199ff912a9eddbe26
Ezrakiel <92792960+Ezrakiel@users.noreply.github.com>
2021-10-19 19:00:21 +0700

    Create BagasHendrayudha.php

---
+++ BagasHendrayudha.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+Tambahan di php 10 ditambah 4 hasil nya? 
+*/
+$a = 10; // variable a kita isi dengan nilai 10
+$b = 4; // variable b kita isi dengan nilai 4
+$c = $a + $b; // disini adalah proses nya
+
+?>
