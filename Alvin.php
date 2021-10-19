1946a3dba17e27b13e6da6e90575184c6a96d3c2
Ibiza77 <74743167+Ibiza77@users.noreply.github.com>
2021-10-19 15:06:26 +0700

    Update Alvin.php
    Add body, css div, and php foreach

--- Alvin.php
+++ Alvin.php
@@ -36,4 +36,23 @@ if (playsCount() >= 9) {
 		.clear { clear: both; }
 	</style>
 </head>
+	<body>
+
+	<?php for( $i = 0; $i < count($angka); $i++ ) { ?>
+	<div class="kotak"><?php echo $angka[$i]; ?></div>
+	<?php } ?>
+
+<div class="clear"></div>
+
+	<?php foreach ($angka as $a) { ?>
+	<div class="kotak"><?php echo $a; ?></div>
+	<?php } ?>
+
+	<div class="clear"></div>
+
+	<?php foreach ( $angka as $a ) : ?>
+	<div class="kotak"><?= $a; ?></div>
+	<?php endforeach ?>
+	
+</body>
 </html>
