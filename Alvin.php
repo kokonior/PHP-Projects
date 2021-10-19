7fc62dcd7c9aef9fa905821005ac1ed7b1ebcf76
Ibiza77 <74743167+Ibiza77@users.noreply.github.com>
2021-10-19 15:04:55 +0700

    Update Alvin.php
    Add html in my php programs

--- Alvin.php
+++ Alvin.php
@@ -17,3 +17,23 @@ if (playsCount() >= 9) {
     header("location: result.php");
 }
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Exanthenon</title>
+	<style>
+		.kotak {
+			width: 50px;
+			height: 50px;
+			background-color: salmon;
+			text-align: center;
+			line-height: 50px;
+			margin: 3px;
+			float: left;
+		}
+		.clear { clear: both; }
+	</style>
+</head>
+</html>
