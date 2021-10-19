5988bf47133461a8f61fc6daf6ab9a8937dd51f0
afifmufty <47510145+afifmufty@users.noreply.github.com>
2021-10-19 20:50:51 +0700

    Update AfifMufty.php
    Add some features

--- AfifMufty.php
+++ AfifMufty.php
@@ -13,4 +13,16 @@ if ($_POST['cell']) {
     }
 }
 
+if (playsCount() >= 9) {
+    header("location: result.php");
+}
+?>
+
+<h2><?php echo currentPlayer() ?>'s turn</h2>
+
+<form method="post" action="play.php">
+
+    <table class="tic-tac-toe" cellpadding="0" cellspacing="0">
+        <tbody>
+            
 ?>
