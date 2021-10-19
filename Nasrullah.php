bef6affc0e7deea27ac55e659197d5cc79ee275d
Nasrullah M. Haris <Nasrullahharis99@gmail.com>
2021-10-19 20:43:07 +0700

    Update Nasrullah.php
    Add div

--- Nasrullah.php
+++ Nasrullah.php
@@ -16,4 +16,19 @@
 		.clear { clear: both; }
 	</style>
 </head>
+<body>
+
+	<?php for( $i = 0; $i < count($angka); $i++ ) { ?>
+	<div class="kotak"><?php echo $angka[$i]; ?></div>
+	<?php } ?>
+
+	<div class="clear"></div>
+
+	<?php foreach ($angka as $a) { ?>
+	<div class="kotak"><?php echo $a; ?></div>
+	<?php } ?>
+
+	
+</body>
+</html>
 </html>
