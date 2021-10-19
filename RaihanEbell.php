3ccf4681b428a51b1e56d31d36064d543e0b30cc
projectradin <92783449+projectradin@users.noreply.github.com>
2021-10-19 15:27:58 +0700

    Create RaihanEbell.php

---
+++ RaihanEbell.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+Tambahan di php 10 ditambah 4 hasil nya? 
+*/
+$a = 10; // variable a kita isi dengan nilai 10
+$b = 4; // variable b kita isi dengan nilai 4
+$c = $a + $b; // disini adalah proses nya
+echo "Hasil dari Tambahan $a + $b = $c"; // disini outputnya
+echo "=======================";
+/*
+
+?>
