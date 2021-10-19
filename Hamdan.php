b27538703a9de14b4069b101a4023fa6ec7670ee
ilhamgetsuga <54354327+ilhamgetsuga@users.noreply.github.com>
2021-10-19 23:55:32 +0700

    Update Hamdan.php
    when og sir

--- Hamdan.php
+++ Hamdan.php
@@ -28,4 +28,61 @@ echo "\n";
         echo "\n";
         echo "Kelereng Kamu : ".$kelerengUser;
         echo "\n";
+        
+        do{
+            echo "\n";
+
+            //Get Tebakan User
+            echo "Masukan Tebakan (Ganjil/Genap) : ";
+            $tebakanUser = strtolower(trim(fgets(STDIN)));
+            
+            //Get Taruhan User
+            echo "Masukan Taruhan 1 - 10         : ";
+            $taruhanUser = trim(fgets(STDIN));
+            echo "\n";
+
+            //Get Rand number for bot
+            $taruhanKelereng = rand(1,$kelerengBot);
+            
+            //cek ganjil genap
+            if($taruhanKelereng % 2 == 0 ){
+                $tebakanBot = "genap";
+            }else{
+                $tebakanBot = "ganjil";
+            }
+
+
+            //main function
+            if($tebakanUser == $tebakanBot){
+
+                //update when user win
+                $kelerengBot  -= $taruhanUser;
+                $kelerengUser += $taruhanUser;
+
+                echo "Tebakan Kamu Benar \n";
+                echo "Tebakan Bot   : ".$tebakanBot."(".$taruhanKelereng.")";
+                echo "\n";
+                echo "Tebakan Kamu  : ".$tebakanUser;
+                echo "\n\n";
+                echo "Kelereng Bot  : ".$kelerengBot;
+                echo "\n";
+                echo "Kelereng Kamu : ".$kelerengUser;
+
+            }else{
+
+                //update when user lose
+                $kelerengBot  += $taruhanUser;
+                $kelerengUser -= $taruhanUser;
+
+                echo "Tebakan Kamu Salah \n";
+                echo "Tebakan Bot   : ".$tebakanBot."(".$taruhanKelereng.")";
+                echo "\n";
+                echo "Tebakan Kamu  : ".$tebakanUser;
+                echo "\n\n";
+                echo "Kelereng Bot  : ".$kelerengBot;
+                echo "\n";
+                echo "Kelereng Kamu : ".$kelerengUser;
+                echo "\n";
+                
+            }
 ?>
