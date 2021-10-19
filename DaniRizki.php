b281583ea4d883bd229ed707d19ca82bed9a4968
Ahmad Dani Rizki <92778561+danirizki17@users.noreply.github.com>
2021-10-19 14:22:37 +0700

    Update DaniRizki.php
    Add html syntax

--- DaniRizki.php
+++ DaniRizki.php
@@ -3,3 +3,22 @@
 $angka = [3,2,15,20,11,77,89,8,7];
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Exanthenon</title>
+	<style>
+		.kotak {
+			width: 50px;
+			height: 50px;
+			background-color: salmon;
+			text-align: center;
+			line-height: 50px;
+			margin: 3px;
+			float: left;
+		}
+		.clear { clear: both; }
+	</style>
+</head>
