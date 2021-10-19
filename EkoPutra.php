0bb55852fb54e043b5610f119e275e5abef38848
ekoputrar <44194770+ekoputrar@users.noreply.github.com>
2021-10-19 19:15:02 +0700

    Create EkoPutra.php

---
+++ EkoPutra.php
@@ -0,0 +1,14 @@
+<?php
+
+    //set kelereng awal
+    $kelerengBot  = 10;
+    $kelerengUser = 10;
+
+    //Deklarasi
+    $namaUser;
+    $tebakanUser;
+    $taruhanUser;
+    $taruhanKelereng;
+    $tebakanBot;
+
+?>
