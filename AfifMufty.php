6a99c5626cd5e221930cdc08fbcf879960caca4e
afifmufty <47510145+afifmufty@users.noreply.github.com>
2021-10-19 20:52:20 +0700

    Update AfifMufty.php
    Javascript

--- AfifMufty.php
+++ AfifMufty.php
@@ -64,3 +64,20 @@ if (playsCount() >= 9) {
             </td>
 
         <?php } ?>
+
+            </tr>
+        </tbody>
+    </table>
+
+    <button type="submit" disabled id="play-btn">Play</button>
+
+</form>
+
+<script type="text/javascript">
+    function enableButton() {
+        document.getElementById('play-btn').disabled = false;
+    }
+</script>
+
+<?php
+require_once "templates/footer.php";
