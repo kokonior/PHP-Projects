ebe5e25aa2cf41607d1b4482ef3911144e3db684
akusiyas <86578358+akusiyas@users.noreply.github.com>
2021-10-19 16:16:10 +0700

    Update MuhammadYasir.php
    Add function and return php

--- MuhammadYasir.php
+++ MuhammadYasir.php
@@ -8,4 +8,36 @@ function salam($date = 'Selamat Datang', $name = 'admin') {
 	return "Selamat $date, $name";
 }
 
+$x = 9;
+$y = 3;
+
+
+
+echo "=================================== <br/>";
+// FUngsi penjumlahan
+function functionPenambahan($x, $y)
+{
+    $HasilPenambahan = $x + $y;
+    return $HasilPenambahan;
+}
+// FUngsi Perkalian
+function functionPerkalian($x, $y)
+{
+    $HasilPerkalian = $x * $y;
+    return $HasilPerkalian;
+}
+// Fungsi Pengurangan
+function functionPengurangan($x, $y)
+{
+    $HasilPengurangan = $x - $y;
+    return $HasilPengurangan;
+}
+// Fungsi Pembagian
+function functionPembagian($x, $y)
+{
+    $HasilPembagian = $x / $y;
+    return $HasilPembagian;
+}
+
 ?>
+
