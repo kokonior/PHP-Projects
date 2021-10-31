2c163e23aa040c54a0dc0fd18aec9a92a28bdfc2
taufiqtaufiq <93473916+taufiqtaufiq@users.noreply.github.com>
2021-10-31 18:48:05 +0700

    Update
    Update

--- 1.php
+++ 1.php
@@ -7,7 +7,7 @@ echo "Today is".date("Y.m.d").;
 echo "Today is".date("Y-m-d").;
   echo"tomorrow is".date("y/m/d").:
   echo"yesterday is".date("y/m/d").:
-
+echo "Ini Hacktober Bro ";
 ?>
 
 </body>
