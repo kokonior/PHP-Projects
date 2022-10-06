32d26dad1d0672279766dc1d155d79fd9583b82c
Hayyi Shinshan Mahesa <hayyi123sm@gmail.com>
2022-10-06 21:09:25 +0800

    adding file prak5.php

---
+++ prak5.php
@@ -0,0 +1,21 @@
+<?php
+$a = "5";
+$b = "2.5";
+$komentar = "Selamat Datang";
+echo ("Nilai variabel a adalah = $a <br>");
+//variabel bertipe integer
+echo ("Nilai variabel b adalah = $b <br>");
+//variabel bertipe real 
+echo ("Nilai variabel komentar adalah =
+$komentar<br>");
+//variabel bertipe string
+$hasil = $a + $b;
+echo ("Hasil jumlah a dan b adalah = $hasil <br>");
+//variabel bertipe double
+$nama = "STMIK AKAKOM";
+$garis = "=====================================";
+echo "<p>";
+echo $garis . "<br>";
+echo $komentar . " Di Lab " . $nama . "<br>Belajar
+dengan giat ya.... <br>";
+echo $garis . "<br>";
