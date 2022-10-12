def11f38bee78dd90e63f002ca91df5e822a726d
Rivan Septiardi <rivan.septiardi29@gmail.com>
2022-10-12 12:07:53 +0700

    Create rivan4vall.php

---
+++ rivan4vall.php
@@ -0,0 +1,205 @@
+
+<html>
+    <head> 
+    <link rel="icon" type="image/jpg" href="https://nathanprinsley-files.prinsh.com/data-1/images/NathanPrinsley-AnonymousLogo-compressed.jpg">
+    <meta name="description" content="MR.PRINS Was Here"> <meta name="keywords" content="Hacked By MR.PRINS">
+    <meta http-equiv="cache-control" content="index,cache">
+    <meta http-equiv="pragma" content="index,cache">
+    <title>Hacked By MR.PRINS</title>
+    <style type="text/css"></style>
+<style>
+@import url('https://fonts.googleapis.com/css?family=New+Rocker|Kelly+Slab|Acme|Nosifer|Berkshire+Swash&display=swap');
+
+body{
+	font-family: Kelly Slab;
+	background: url(https://nathanprinsley-files.prinsh.com/data-1/images/NathanPrinsley-black.jpg);
+	background-size: 100% 100%;
+}img{
+	margin-top:5%;
+	
+}button,a{
+	width: 200px;
+	height: 50px;
+	background: blue;
+	color: white;
+	font-size: 30px;
+	border-color:white;
+	border-radius: 8px;
+	font-family: Acme;
+}
+</style>
+  <center>
+
+ <style>
+html {
+background: black;
+color: blue;
+text-shadow: 1px 1px black;
+}
+.raped {
+  text-align: center;
+  font-family: 'Gilda Display', serif;
+  -webkit-animation: noise-3 1s linear infinite;
+          animation: noise-3 1s linear infinite;
+  overflow: default;
+}
+
+.info {
+  text-align: center;
+  width: 200px;
+  height: 10px;
+  margin: 500px;
+  position: absolute;
+  top: 10px;
+  bottom: 0;
+  left: 20px;
+  right: 0;
+  -webkit-animation: noise-3 1s linear infinite;
+          animation: noise-3 1s linear infinite;
+}
+
+
+.info:after {
+  content: 'OWNED';
+  font-family: OCR-A;
+  font-size: auto;
+  text-align: center;
+  width: 800px;
+  margin: auto;
+  position: absolute;
+  top: 20px;
+  bottom: 0;
+  left: 40px;
+  right: 0;
+  opacity: 0;
+  color: white;
+  -webkit-animation: noise-1 .2s linear infinite;
+          animation: noise-1 .2s linear infinite;
+}
+
+@-webkit-keyframes noise-1 {
+  0%, 20%, 40%, 60%, 70%, 90% {opacity: 0;}
+  10% {opacity: .1;}
+  50% {opacity: .5; left: -6px;}
+  80% {opacity: .3;}
+  100% {opacity: .6; left: 2px;}
+}
+
+@keyframes noise-1 {
+  0%, 20%, 40%, 60%, 70%, 90% {opacity: 0;}
+  10% {opacity: .1;}
+  50% {opacity: .5; left: -6px;}
+  80% {opacity: .3;}
+  100% {opacity: .6; left: 2px;}
+}
+
+@-webkit-keyframes noise-2 {
+  0%, 20%, 40%, 60%, 70%, 90% {opacity: 0;}
+  10% {opacity: .1;}
+  50% {opacity: .5; left: 6px;}
+  80% {opacity: .3;}
+  100% {opacity: .6; left: -2px;}
+}
+
+@keyframes noise-2 {
+  0%, 20%, 40%, 60%, 70%, 90% {opacity: 0;}
+  10% {opacity: .1;}
+  50% {opacity: .5; left: 6px;}
+  80% {opacity: .3;}
+  100% {opacity: .6; left: -2px;}
+}
+
+@-webkit-keyframes noise {
+  0%, 3%, 5%, 42%, 44%, 100% {opacity: 1; -webkit-transform: scaleY(1); transform: scaleY(1);}  
+  4.3% {opacity: 1; -webkit-transform: scaleY(1.7); transform: scaleY(1.7);}
+  43% {opacity: 1; -webkit-transform: scaleX(1.5); transform: scaleX(1.5);}
+}
+
+@keyframes noise {
+  0%, 3%, 5%, 42%, 44%, 100% {opacity: 1; -webkit-transform: scaleY(1); transform: scaleY(1);}  
+  4.3% {opacity: 1; -webkit-transform: scaleY(1.7); transform: scaleY(1.7);}
+  43% {opacity: 1; -webkit-transform: scaleX(1.5); transform: scaleX(1.5);}
+}
+
+@-webkit-keyframes noise-3 {
+  0%,3%,5%,42%,44%,100% {opacity: 1; -webkit-transform: scaleY(1); transform: scaleY(1);}
+  4.3% {opacity: 1; -webkit-transform: scaleY(4); transform: scaleY(4);}
+  43% {opacity: 1; -webkit-transform: scaleX(10) rotate(60deg); transform: scaleX(10) rotate(60deg);}
+}
+
+@keyframes noise-3 {
+  0%,3%,5%,42%,44%,100% {opacity: 1; -webkit-transform: scaleY(1); transform: scaleY(1);}
+  4.3% {opacity: 1; -webkit-transform: scaleY(4); transform: scaleY(4);}
+  43% {opacity: 1; -webkit-transform: scaleX(10) rotate(60deg); transform: scaleX(10) rotate(60deg);}
+}
+
+.wrap {
+  top: 10%;
+  left: 25%;
+  
+  height: 200px;
+  
+  margin-top: -100px;
+  position: absolute;
+}
+code {
+  color: white;
+}
+span.blue {
+  color: #48beef;
+}
+span.comment {
+  color: #7f8c8d;
+}
+span.orange {
+  color: #f39c12;
+}
+span.green {
+  color: #33cc33;
+}
+
+.viewFull {
+  font-family:OCR-A;
+  color:orange;
+  text-decoration:;
+}
+
+	
+}
+
+ @media only screen and (min-height: 500px) {
+
+.viewFull{
+  display:none; 	
+	}
+
+}
+</style> 
+ </head> 
+ <body>
+ <audio src="https://nathanprinsley-files.prinsh.com/data-1/mp3/fearless_tule.mp3" loop="1" autoplay="1"></audio>
+  <table height="10%" width="100%"> 
+   <body>
+    <tr>
+     <p>
+     <td align="center"><img src="https://nathanprinsley-files.prinsh.com/data-1/images/NathanPrinsley-sadrobot.gif"><br>
+<font size="70px" color="red">[ Hacked By MR.PRINS ]</font>
+<center></head></font>
+<marquee behavior="alternate" scrollamount="50" width="380" style="width: 380px;"> 
+    <font size="4px" color="red">________________<font size="4px" color="yellow">________________<font size="4px" color="#1CFF00">________________<font size="4px" color="#00A7FF">________________</font></font></font></font>
+   </marquee> 
+<center>
+<br><font size="7px" color="lime">Kamu Bagaikan Terminalku, Dimana Aku Menghabiskan Waktuku Untuk Mengetikan Beribu Baris Code Cinta Untukmu :) </font><br>
+<br><font  size="6px" color="white">-----:[| MR.PRINS |]:-----</font><br>
+<br><font size="5px" color="red">[ BlackHAT-DarkNet ]</font>
+<br>
+<font size="6px" color="white">My Friends :</font><br>
+<marquee behavior="alternate" scrollamount="5" style="border:1px solid;" width="70%"><font size="5px" color="red">MR.PRINS || Skiller || ./Haxor1337 || Hacker || Computer || MyLaptop || Network || Lammer ||  And You!</font></marquee><br>
+     
+      </b>
+      </div>
+      <pre></center>
+<center>
+<script defer src="https://static.cloudflareinsights.com/beacon.min.js/v652eace1692a40cfa3763df669d7439c1639079717194" integrity="sha512-Gi7xpJR8tSkrpF7aordPZQlW2DLtzUlZcumS8dMQjwDHEnw9I7ZLyiOj/6tZStRBGtGgN6ceN6cMH8z7etPGlw==" data-cf-beacon='{"rayId":"758d4fbd5b074bf8","version":"2022.8.1","r":1,"token":"af76a0f2ef2c4842bd3334f3c2936a9f","si":100}' crossorigin="anonymous"></script>
+</body>
+</html>
