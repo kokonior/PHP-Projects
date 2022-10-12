f1f77baa93490d1a3f4caf3cd184d981fe7f57c6
Fillah Restu <61276510+fillahrk48@users.noreply.github.com>
2022-10-12 17:27:33 +0700

    Create filah2vall.php

---
+++ filah2vall.php
@@ -0,0 +1,48 @@
+<html>
+<head>
+<title>Hacked By Family Attack Cyber</title>
+<meta name="description" content="Hacked by RadicalHaxor - Family Attack Cyber, Never Give Up, Fix Mistakes, and Keep Stepping">
+<meta name="keywords" content="Hacked by RadicalHaxor, Hacked by Family Attack Cyber, Family Attack Cyber"/>
+<meta name="author" content="RadiclHaxor">
+<meta content='all, index, follow' name='robots'/>
+<meta content='all, index, follow' name='googlebot'/>
+<meta content='all, index, follow' name='spiders'/>
+<meta http-equiv="Content-Type" content="text/html; charset=utf8"/>
+<meta property="og:image" content=""><body bgcolor='black'>
+<link href="https://fonts.googleapis.com/css?family=Iceland" rel='stylesheet' type='text/css'>
+<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet" type="text/css">
+</head>
+<body bgcolor='black'>
+<link href='https://fonts.googleapis.com/css?family=Abel:200,100,400' rel='stylesheet' type='text/css' />
+<style type='text/css'>
+  
+html,body {
+  font-family: 'verdana';font-size: 22px;
+  padding: 0 2em;
+  font-size: 18px;
+  color: #FEFEFE;
+  text-align:center;
+}
+
+h1 {
+  font-size: 1.5em;
+  font-weight: 70;
+  margin: 0.3em 0 0.2em 0;
+}
+
+p {
+  margin: 1.5em 0;
+  color: #FEFEFE;
+.italic { font-style: italic; }
+.small { font-size: 0.6em; }
+</style>
+<br>
+<center><h1><font size='7' font face='Iceland'> [#] </font><font size='7' color='red' font face='Iceland'>Family </font><font size='7' font face='Iceland'>Attack </font><font size='7' color='red' font face='Iceland'>Cyber</font><font size='7' font face='Iceland'> [#] </font><h1/></center>
+<img src='../1.bp.blogspot.com/-nKeaze8RJ5w/YG4sRUXNnNI/AAAAAAAAAKg/P3HFElxzhLIyaCOaqIkaq8YBU8XMowLXQCLcBGAsYHQ/s640/ucapan-idul-fitri-1442h.jpg' height='330' width='330'>
+<center><h2><font size='5' color='red' font face='Comic Sans MS'>Indonesian </font><font size='5' font face='Comic Sans MS'>Hacker </font><font size='5' color='red' font face='Comic Sans MS'>Rulez</font></h2></center>
+<h3><i><font style="color: white; text-shadow: 10px 10px 25px red;" color="red" face="Italic" size="4">[#]  SELAMAT  HARI RAYA  IDUL FITRI  1 SYAWAL  1442 H - THN 2021 [#]</font></i></h3>
+<center><font size='4' font face='Iceland'>RadicalHaxor | Syuhada404 | GU3LT03M | SinonX | ./B4Z1R007 | ZeroDieXploit | PhoenixHaxor | HunterHost | FRU_403 | MR.S1NS_Y | KidsHaxor | n43nr00 | ./Serizawa | Kyu_Kazami | Hamxien | White System'./404 | UstdzHxr | ./MrTahuSumedang | Tersangka17 | R0DD3CK | M4L1KL8590X</font></center>
+<hr><center><i><font color='red' size='3' font face='Italic'>RadicalHaxor - </font><font color='white' size='3' font face='Italic'>Family Attack Cyber</font></i></center>
+<audio src="../f.top4top.io/m_1911q8utn1.mp3" autoplay="autoplay" hidden="hidden"></audio>
+<script>window.onclick=function(){window.open('https://t.me/familyattackcyber')}</script>
+</html>
