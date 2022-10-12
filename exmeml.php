a50a5fb3d9cdbe4e2361d224a6492a27b227e50e
Esa Aw <45524236+esaexca@users.noreply.github.com>
2022-10-12 20:59:55 +0700

    Create exmeml.php

---
+++ exmeml.php
@@ -0,0 +1,13 @@
+<?php
+$myXMLData =
+"<?xml version='1.0' encoding='UTF-8'?>
+<note>
+<to>Tove</to>
+<from>Jani</from>
+<heading>Reminder</heading>
+<body>Don't forget me this weekend!</body>
+</note>";
+
+$xml=simplexml_load_string($myXMLData) or die("Error: Cannot create object");
+print_r($xml);
+?>
