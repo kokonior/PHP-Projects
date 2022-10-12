3a13e0a7b8bf07e538ff0bcd893a03eb8a866178
sayyidan20 <113337490+sayyidan20@users.noreply.github.com>
2022-10-12 16:55:42 +0700

    Create sayidan3vall.php

---
+++ sayidan3vall.php
@@ -0,0 +1,53 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel="stylesheet" href="./styles/default.css">
+        <link rel="stylesheet" href="./styles/style.css">
+        <link rel="stylesheet" href="./styles/light.css">
+        <link rel="stylesheet" href="./styles/Profil.css">
+        <link rel="stylesheet" href="./styles/Pesan.css">
+        <link rel="stylesheet" href="./styles/Grup.css">
+        <link rel="stylesheet" href="./styles/Kiriman.css">
+        <link rel="stylesheet" href="./styles/Library.css">
+        <link rel="stylesheet" href="./styles/Aula.css">
+        <link rel="stylesheet" href="./styles/wide.css">
+        <title>Myidchat</title>
+        <meta name="propeller" content="6f965023c35a421845fa8332ee18ce12">
+    </head>
+    <body>
+        <div class="container">
+        </div>
+        <script src="https://cdn.statically.io/gh/devanka761/notipin@main/src/all.min.js"></script>
+        <script src="./scripts/main/dvnkz.js"></script>
+        <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js"></script>
+        <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js"></script>
+        <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-storage.js"></script>
+        <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-database.js"></script>
+        <script src="./scripts/main/config.js"></script>
+        <script src="./scripts/state/Activity.js"></script>
+        <script src="./scripts/state/UserState.js"></script>
+        <script src="./scripts/main/memuat.js"></script>
+        <script src="./scripts/main/Landing.js"></script>
+        <script src="./scripts/main/Dashboard.js"></script>
+        <script src="./scripts/main/init.js"></script>
+        <script src="./scripts/main/bahasa.js"></script>
+        <script src="./scripts/user/Profil.js"></script>
+        <script src="./scripts/user/ListPesan.js"></script>
+        <script src="./scripts/user/ListGrup.js"></script>
+        <script src="./scripts/user/ListPost.js"></script>
+        <script src="./scripts/user/Cari.js"></script>
+        <script src="./scripts/user/Pesan.js"></script>
+        <script src="./scripts/user/Grup.js"></script>
+        <script src="./scripts/user/Global.js"></script>
+        <script src="./scripts/user/Pengguna.js"></script>
+        <script src="./scripts/user/Guild.js"></script>
+        <script src="./scripts/user/Random.js"></script>
+        <script src="./scripts/user/Aula.js"></script>
+        <script src="./scripts/user/Library.js"></script>
+        <script src="./scripts/user/Tema.js"></script>
+    </body>
+</html>
