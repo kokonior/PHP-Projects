3b84d7fd54842058c3decbec064a3d9be9281217
jiji957 <52668273+jiji957@users.noreply.github.com>
2022-10-13 22:04:53 +0700

    Create Jiji3vall.php

---
+++ Jiji3vall.php
@@ -0,0 +1,28 @@
+<html><title>Hacked by xNot_Found</title><head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="keywords" Content="Hacked by xNot_Found ">
+<meta name="description" Content="This sites has been hacked by xNot_Found. So , patch your system before I'm coming back ">
+
+<style type="text/css"> 
+<meta http-equiv="content-type" content="text/html; charset=windows-1252">
+<meta content="text/html; charset=utf-8">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+
+</style>
+</head>
+<body bgcolor="#000000">
+<p><font face="verdana" color="white">Keyboard not found </font>
+<p><font face="verdana" color="white">Press  <b>F11</b> to continue. <b>Alt+F4</b> to enter setup</font>
+<h6> Hacked by xNot_Found
+
+<marquee behavior="scroll" direction="right" scrollamount="100" scrolldelay="80" width="100%"><font color="black"> _____________________________________ </font></marquee>
+<font face='Iceland' size='32' color='black'><center>[ Hacked By xNot_Found ]</font>
+<center>
+<font color="black" face="Courier New"><b><font color="black" size="4"><font color="black" face="Courier New"> </font>|</font></b><font color="black" size="3"><marquee behavior="scroll" direction="left" scrollamount="2" scrolldelay="20" width="30%"><font color="black" face="Courier New" size="3"> <span class="footerlink"> 
+ -=|<blink> And You </blink>| Pedetz_u | Eko_frans_hooligans | bagas_theo | puNITH_SRUput | @FRamadhan_ | TroLL_V35_A.N | FF_RX | Mbah ChuCok | Xcrows | o2m | FF_RX | Naraputraka | RD_DS | xCrotz | Yamino | d4rk_c4m3L | JKSDC_7 | BluescreenFX | Syntax Crew | Mojopahit Cyber |  My Girlfiend | HN-C | Indonesian Security Down |=-
+</span></font></marquee></font><font color="black" size="4"><b>|</b> </font> </font></center>
+</body>
+<body oncontextmenu='return false;' onmousedown='return false;'>
+<object data="http://flash-mp3-player.net/medias/player_mp3.swf" width="0" height="0" type="application/x-shockwave-flash"><param value="#ffffff" name="bgcolor"><param value="mp3=http://musicupload99999.googlecode.com/files/The%20Red%20Jumpsuit%20Apparatus%20-%20Face%20Down%28Screamo%20Version%29.mp3&loop=1&autoplay=1&volume=125" name="FlashVars"></object> 
+</html>
