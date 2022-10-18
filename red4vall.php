41207511672aa3857cacaa5f76e3dd989d0b6fd5
DyanHeluka <41682742+DyanHeluka@users.noreply.github.com>
2022-10-18 12:26:28 +0700

    Create red4vall.php

---
+++ red4vall.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+
+<title> Latihan HTML </title>
+
+<head>
+
+<body>
+
+<body bgcolor=#00FFFF>
+
+<h1> Belajar Website </h1>
+
+<hr width=100% align=center size=5% color=”blue”>
+
+<h2><b>HURUF TEBAL</b></h2>
+
+<p><i>Huruf Miring</i><br><u>Garis Bawah</u> </p>
