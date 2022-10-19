f8cfc29dcb194c3166b2cf5867a5fa42ffdd0781
humanoid <88983316+Islahnrhd@users.noreply.github.com>
2022-10-19 23:15:16 +0700

    Create melon4vall.php

---
+++ melon4vall.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html&gt;
+<html lang="en"&gt;
+<head&gt;
+    <meta charset="UTF-8"&gt;
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"&gt;
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"&gt;
+    <title&gt;E-Commerce</title&gt;
+    <link rel="stylesheet" href="css/tampilan.css"&gt;
+</head&gt;
+<body&gt;
+     
+    <div class="badan"&gt;
+        <h2&gt;Produk Baru</h2&gt;
+ 
+        <div class="list-produk"&gt;
+            <img src="gambar/jaket-parasut.jpg" alt="Jaket Parasut"&gt;
+ 
+            <h4&gt;Jaket Parasut Polos</h4&gt;
+            <h5&gt;Rp 150.000,-</h5&gt;
+ 
+            <a class="tombol tombol-detail" href="#"&gt;Detail</a&gt; 
+            <a class="tombol tombol-beli" href="#"&gt;Beli</a&gt;
+        </div&gt;
+ 
+        <div class="list-produk"&gt;
+            <img src="gambar/jaket-tactical.jpg" alt="Jaket Tactical"&gt;
+ 
+            <h4&gt;Jaket Tactical Hitam</h4&gt;
+            <h5&gt;Rp 230.000,-</h5&gt;
+ 
+            <a class="tombol tombol-detail" href="#"&gt;Detail</a&gt; 
+            <a class="tombol tombol-beli" href="#"&gt;Beli</a&gt;
+        </div&gt;
+ 
+        <div class="list-produk"&gt;
+            <img src="gambar/bomber-stylish.jpg" alt="Jaket Bomber Stylish"&gt;
+ 
+            <h4&gt;Jaket Bomber Stylish</h4&gt;
+            <h5&gt;Rp 250.000,-</h5&gt;
+ 
+            <a class="tombol tombol-detail" href="#"&gt;Detail</a&gt; 
+            <a class="tombol tombol-beli" href="#"&gt;Beli</a&gt;
+        </div&gt;
+ 
+        <div class="list-produk"&gt;
+            <img src="gambar/jaket-naruto.jpg" alt="Jaket Naruto"&gt;
+ 
+            <h4&gt;Jaket Naruto</h4&gt;
+            <h5&gt;Rp 170.000,-</h5&gt;
+ 
+            <a class="tombol tombol-detail" href="#"&gt;Detail</a&gt; 
+            <a class="tombol tombol-beli" href="#"&gt;Beli</a&gt;
+        </div&gt;
+    </div&gt;
+ 
+</body&gt;
+</html&gt;
